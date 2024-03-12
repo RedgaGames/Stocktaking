5a816539dcf7de5708bda761c64e798df57d5f3e
Redga_Games <games@redga.de>
2024-03-12 08:19:33 +0100

    Added more stuff to the scene

--- Assets/_Game/_Scripts/Level/Level 1/DirectorLevel1.cs
+++ Assets/_Game/_Scripts/Level/Level 1/DirectorLevel1.cs
@@ -69,12 +69,12 @@ public class DirectorLevel1 : MonoBehaviour
         outroTextHandler.AddTextLineToDialog("Hello fresh meat.");
         // outroTextHandler.AddTextLineToDialog("Welcome to the X Cooperation.");
         // outroTextHandler.AddTextLineToDialog("Your job is to do our inventory.");
-        // outroTextHandler.AddTextLineToDialog("Make notes of our current stock \nand then finish your shift.");
-        // outroTextHandler.AddTextLineToDialog("And try not to do too much wrong otherwise \nMr. Willson will be very unhappy.");
-        // outroTextHandler.AddTextLineToDialog("And if Mr. Willson is unhappy, \nyou are going to be unhappy.");
-        // outroTextHandler.AddTextLineToDialog("No worries. Im just joking...");
-        // outroTextHandler.AddTextLineToDialog("Anyway.\nHave a great first shift!!!");
-
+        //  outroTextHandler.AddTextLineToDialog("Make notes of our current stock \nand then finish your shift.");
+        //  outroTextHandler.AddTextLineToDialog("And try not to do too much wrong otherwise \nMr. Willson will be very unhappy.");
+        //  outroTextHandler.AddTextLineToDialog("And if Mr. Willson is unhappy, \nyou are going to be unhappy.");
+        //  outroTextHandler.AddTextLineToDialog("No worries. Im just joking...");
+        //  outroTextHandler.AddTextLineToDialog("Anyway.\nHave a great first shift!!!");
+         
         outroTextHandler.SetMaskEmotionForOutro(OutroTextHandler.MaskGuyEmotionOutro.happy);
     }
 
